d7e41b1282c2a4a4c7b9a7af49a9e5b5fc4878db
lanten <lanten233@qq.com>
2019-12-29 18:31:46 +0800

    版本回滚

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   createByTemplate: require('./createByTemplate'),
-  createFileByTemplate: require('./createFileByTemplate'),
   editTemplateGlobal: require('./editTemplateGlobal'),
   editTemplateWorkspace: require('./editTemplateWorkspace'),
 }
